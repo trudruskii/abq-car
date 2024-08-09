9ad14c45a7681da67db3d9b97cecb3032fec69ef
trudrewski <aspencer22@cnm.edu>
2024-08-09 01:59:32 -0600

    adjusted logo -- site name to the left with absolute position -- will probably need to adjust that to something else

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -19,8 +19,8 @@ export default function Navbar() {
         <>
             <nav className="bg-white border-gray-200 dark:bg-gray-900 relative">
                 <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                    <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
-                        <img src="/placeholder.logo.svg" className="h-24 rounded-full" alt="Flowbite Logo"/>
+                    <a href="/" className="flex items-center -ml-56 rtl:space-x-reverse gap-6">
+                        <img src="/placeholder.logo.svg" className="h-32 rounded-full" alt="ABQ-CAR Logo"/>
                         <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
                             Albuquerque - Catch a Ride
                         </span>
@@ -28,7 +28,7 @@ export default function Navbar() {
                     <div className="relative flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
                         <button
                             type="button"
-                            className="flex text-sm bg-gray-800 rounded-full md:me-0 dark:focus:ring-gray-600"
+                            className="flex text-xs bg-gray-800 rounded-full md:me-0 dark:focus:ring-gray-600"
                             onClick={toggleUserMenu}
                         >
                             <span className="sr-only">Open user menu</span>
@@ -83,106 +83,109 @@ export default function Navbar() {
                                       d="M1 1h15M1 7h15M1 13h15"/>
                             </svg>
                         </button>
+
                     </div>
 
-
-                    <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1">
-                        <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-                            <li>
-                                <a href="#"
-                                   className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
-                                   aria-current="page">
-                                    Home
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#"
-                                   className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
-                                    About
-                                </a>
-                            </li>
-                            <li>
-                                <button
-                                    type="button"
-                                    className="flex items-center justify-between w-full py-2 px-3 text-gray-900 rounded md:w-auto hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-600 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700"
-                                    onClick={toggleCompanyMenu}
-                                >
-                                    Company
-                                    <svg className="w-2.5 h-2.5 ms-2.5" aria-hidden="true"
-                                         xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"
-                                              strokeWidth="2" d="m1 1 4 4 4-4"/>
-                                    </svg>
-                                </button>
-                            </li>
-                            <li>
-                                <a href="#"
-                                   className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700">
-                                    Marketplace
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#"
-                                   className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700">
-                                    Resources
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#"
-                                   className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700">
-                                    Contact
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#"
-                                   className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
-                                    Pricing
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                    {isCompanyMenuOpen && (
-                        <div id="mega-menu-full-dropdown"
-                             className="absolute left-1/2 top-full mt-2 w-64 transform -translate-x-1/2 z-50 border-gray-200 shadow-sm bg-gray-50 md:bg-white border-y dark:bg-gray-800 dark:border-gray-600">
-                            <div
-                                className="grid max-w-screen-xl px-4 py-5 mx-auto text-gray-900 dark:text-white sm:grid-cols-2 md:px-6">
-                                <ul>
-                                    <li>
-                                        <a href="#"
-                                           className="block p-3 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700">
-                                            <div className="font-semibold">Online Stores</div>
-                                            <span className="text-sm text-gray-500 dark:text-gray-400">Connect with third-party tools that you're already using.</span>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#"
-                                           className="block p-3 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700">
-                                            <div className="font-semibold">Segmentation</div>
-                                            <span className="text-sm text-gray-500 dark:text-gray-400">Connect with third-party tools that you're already using.</span>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#"
-                                           className="block p-3 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700">
-                                            <div className="font-semibold">Marketing CRM</div>
-                                            <span className="text-sm text-gray-500 dark:text-gray-400">Connect with third-party tools that you're already using.</span>
-                                        </a>
-                                    </li>
-                                </ul>
-                                <ul>
-                                    <li>
-                                        <a href="#"
-                                           className="block p-3 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700">
-                                            <div className="font-semibold">Online Stores</div>
-                                            <span className="text-sm text-gray-500 dark:text-gray-400">Connect with third-party tools that you're already using.</span>
-                                        </a>
-                                    </li>
-                                </ul>
-                            </div>
+                        <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1">
+                            <ul className="flex flex-col text-xl font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                                <li>
+                                    <a href="#"
+                                       className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
+                                       aria-current="page">
+                                        Home
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#"
+                                       className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+                                        About
+                                    </a>
+                                </li>
+                                <li>
+                                    <button
+                                        type="button"
+                                        className="flex items-center justify-between w-full py-2 px-3 text-gray-900 rounded md:w-auto hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-600 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700"
+                                        onClick={toggleCompanyMenu}
+                                    >
+                                        Services
+                                        <svg className="w-2.5 h-2.5 ms-2.5" aria-hidden="true"
+                                             xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"
+                                                  strokeWidth="2" d="m1 1 4 4 4-4"/>
+                                        </svg>
+                                    </button>
+                                </li>
+                                <li>
+                                    <a href="#"
+                                       className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700">
+                                        Marketplace
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#"
+                                       className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700">
+                                        Resources
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#"
+                                       className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700">
+                                        Contact
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#"
+                                       className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+                                        Pricing
+                                    </a>
+                                </li>
+                            </ul>
                         </div>
-                    )}
-                </div>
+
+                        {isCompanyMenuOpen && (
+                            <div id="mega-menu-full-dropdown"
+                                 className="absolute left-1/2 top-28 mt-2 w-80 transform -translate-x-1/2 z-50 border-gray-200 shadow-lg bg-gray-50 md:bg-white border-y dark:bg-gray-800 dark:border-gray-600">
+                                <div
+                                    className="grid grid-cols-1 md:grid-cols-1 gap-4 px-4 mx-auto text-gray-900 dark:text-white md:px-6">
+                                    <ul className="space-y-4">
+                                        <li>
+                                            <a href="#"
+                                               className="block p-3 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700">
+                                                <div className="font-semibold">Online Stores</div>
+                                                <span className="text-sm text-gray-500 dark:text-gray-400">Connect with third-party tools that you're already using.</span>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#"
+                                               className="block p-3 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700">
+                                                <div className="font-semibold">Segmentation</div>
+                                                <span className="text-sm text-gray-500 dark:text-gray-400">Connect with third-party tools that you're already using.</span>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#"
+                                               className="block p-3 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700">
+                                                <div className="font-semibold">Marketing CRM</div>
+                                                <span className="text-sm text-gray-500 dark:text-gray-400">Connect with third-party tools that you're already using.</span>
+                                            </a>
+                                        </li>
+                                    </ul>
+                                    <ul className="space-y-4">
+                                        <li>
+                                            <a href="#"
+                                               className="block p-3 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700">
+                                                <div className="font-semibold">Online Stores</div>
+                                                <span className="text-sm text-gray-500 dark:text-gray-400">Connect with third-party tools that you're already using.</span>
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        )}
+
+                    </div>
             </nav>
+
         </>
-    );
+);
 }
